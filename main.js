878e4e4c9056efd50c3687a87856c77f4092a23b
Soham Samanta <65817108+Soham2020sam@users.noreply.github.com>
2022-07-19 17:50:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// more complete example here: https://github.com/bandaloo/simple-fragment-shader 
-
+/** glsl source code for simplest possible vertex shader */
 const VERTEX_SHADER_SRC = `#version 300 es
 in vec2 aPosition;
 void main() {
   gl_Position = vec4(aPosition, 0.0, 1.0);
 }`;
 
+/** fragment shader adapted from shadertoy */
 const FRAGMENT_SHADER_SRC = `#version 300 es
 precision mediump float;
 out vec4 fragColor;
@@ -14,67 +14,97 @@ uniform vec2 uResolution;
 void main(){
   vec2 uv = gl_FragCoord.xy / uResolution.xy;
   vec3 col = 0.5 + 0.5*cos(uTime+uv.xyx+vec3(0,2,4));
-  fragColor = vec4(col, 1.0);
-  fragColor = vec4(1, 0, 0, 1);
+  fragColor = vec4(col,1.0);
 }`;
+
+// n.b. the `#version` pragma in the each shader has to be the very first line.
+// there can't be any kind of whitespace before it or it will break!
 
 const RES_WIDTH = 1920;
 const RES_HEIGHT = 1080;
 
+/** helper function to log shader compilation errors */
+const shaderLog = (
+  /** @type {string} */ name,
+  /** @type {WebGLShader} */ shader
+) => {
+  const output = gl.getShaderInfoLog(shader);
+  if (output !== "") console.log(`${name} shader info log\n${output}`);
+};
+
 const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("gl"));
 const gl = canvas.getContext("webgl2");
-
-if (gl === null) throw new Error("gl was null!");
-
 canvas.width = RES_WIDTH;
 canvas.height = RES_HEIGHT;
 
+// define drawing area of canvas
 gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
 
+// create a buffer object to store vertices
 const buffer = gl.createBuffer();
 
+// point buffer at graphic context's ARRAY_BUFFER
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 
+// vertices for two big triangles
 const verts = [-1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1];
 const triangles = new Float32Array(verts);
 
+// initialize memory for buffer, populate it and give opengl hint that contents
+// will not change dynamically
 gl.bufferData(gl.ARRAY_BUFFER, triangles, gl.STATIC_DRAW);
 
+// create vertex shader
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-if (vertexShader === null) throw new Error("vertex shader was null!");
-
 gl.shaderSource(vertexShader, VERTEX_SHADER_SRC);
 gl.compileShader(vertexShader);
+shaderLog("vertex", vertexShader);
 
+// create fragment shader
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-if (fragmentShader === null) throw new Error("fragment shader was null!");
-
 gl.shaderSource(fragmentShader, FRAGMENT_SHADER_SRC);
 gl.compileShader(fragmentShader);
+shaderLog("fragment", fragmentShader);
 
+// create shader program
 const program = gl.createProgram();
-if (program === null) throw new Error("program was null!");
-
 gl.attachShader(program, vertexShader);
 gl.attachShader(program, fragmentShader);
 gl.linkProgram(program);
 gl.useProgram(program);
 
+// n.b. all attribute and uniform initialization must come after creating,
+// linking and using the shader program
+
+// get location of the uniform `uTime` in our fragment shader
 const uTime = gl.getUniformLocation(program, "uTime");
 
+// get location of the uniform `uResolution` in our fragment shader
 const uResolution = gl.getUniformLocation(program, "uResolution");
 gl.uniform2f(uResolution, gl.drawingBufferWidth, gl.drawingBufferHeight);
 
+// get location of the attribute `aPosition` in our vertex shader
 const position = gl.getAttribLocation(program, "aPosition");
 
+// enable the attribute
 gl.enableVertexAttribArray(position);
 
+// this will point to the vertices in the last bound array buffer. here we're
+// only use one array buffer, where we're storing our vertices
 gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
 
-const render = (time) => {
+/** renders to the screen, updating the time, calling itself repeatedly */
+const render = (/** @type {number} */ time) => {
+  // update time on the GPU
   gl.uniform1f(uTime, time / 1000);
+
+  // draw triangles using the array buffer from index 0 to 6
   gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+  // schedules render to be called the next time the video card requests
+  // a frame of video
   requestAnimationFrame(render);
 };
 
+// kick off the animation loop
 requestAnimationFrame(render);
